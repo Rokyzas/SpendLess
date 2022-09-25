1b87206e086ec835870bea871f16c58b84cb0984
Danielius <100567065+Danielius4@users.noreply.github.com>
2022-09-25 23:43:30 +0300

    Deleted trash files

--- SpendlessBlazor/SpendlessBlazor/Program.cs
+++ SpendlessBlazor/SpendlessBlazor/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using SpendlessBlazor.Data;
 using MudBlazor.Services;
 
 
@@ -10,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddHttpClient();
 builder.Services.AddMudServices();
 
