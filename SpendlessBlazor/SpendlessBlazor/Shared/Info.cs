cbc7081dc4be0861811ee5dc11a41ee0a532faff
Danielius <100567065+Danielius4@users.noreply.github.com>
2022-10-10 14:49:12 +0300

    Created Enum

--- SpendlessBlazor/SpendlessBlazor/Shared/Info.cs
+++ SpendlessBlazor/SpendlessBlazor/Shared/Info.cs
@@ -6,14 +6,21 @@
         public string? textValue { get; set; }
         public double amount { get; set; }
 
-        public string? categoryValue { get; set; }
+        public int? categoryValue { get; set; }
 
-        public Info(string? textValue, double amount, string? categoryValue, DateTime? date)
+        public Info(string? textValue, double amount, int? categoryValue, DateTime? date)
         {
             this.textValue = textValue;
             this.amount = amount;
             this.categoryValue = categoryValue;
             this.date = date;
         }
+    }
+    public enum categoryValues
+    {
+        Housing, Transportation, Food, Utilities, Investing,
+        Household, Personal, PersonalDevelopment, Gifts_Or_Donations,
+        Entertainment, Medical_Or_Healthcare, Insurance, Kids,
+        Pets, Subscriptions, Clothing, Travel, Technology
     }
 }
