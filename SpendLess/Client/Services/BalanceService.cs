552047d4c804087d38e99d0864255b654b8fcb3c
Danielius <100567065+Danielius4@users.noreply.github.com>
2022-11-15 22:11:40 +0200

    Added refreshBalance

---
+++ SpendLess/Client/Services/BalanceService.cs
@@ -0,0 +1,20 @@
+﻿using SpendLess.Shared;
+using SpendLess.Client.Services;
+using SpendLess.Client.Pages;
+using SpendLess.Client.Shared;
+using System.Net.Http;
+
+
+namespace SpendLess.Client.Services
+{
+    public class BalanceService : IBalanceService
+    {
+        public event Action? BalanceChanged;
+
+        public async Task RefreshBalance()
+        {
+            if (BalanceChanged != null)
+                BalanceChanged.Invoke();
+        }
+    }
+}
