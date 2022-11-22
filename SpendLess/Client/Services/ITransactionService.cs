a56310c0cfd748313e3829e0637aca959841e763
Rokas Čebatorius <77296454+Rokyzas@users.noreply.github.com>
2022-11-22 15:20:27 +0200

    delegate example

--- SpendLess/Client/Services/ITransactionService.cs
+++ SpendLess/Client/Services/ITransactionService.cs
@@ -1,8 +1,9 @@
 ﻿//using Microsoft.AspNetCore.Mvc;
 using SpendLess.Shared;
-
 namespace SpendLess.Client.Services
 {
+    public delegate void LogException(HttpClient client, string str, Exception ex);
+
     public interface ITransactionService
     {
         public List<Transactions> Transactions { get; set; }
@@ -10,7 +11,7 @@ namespace SpendLess.Client.Services
         event EventHandler<EventArgs>? TransactionsChanged;
 
         public Task OnTransactionsChanged();
-
+        Task GetTransactions(LogException logexception);
         Task GetTransactions();
 
         Task AddTransaction(double? amount, string category, DateTime date, string comment = "Transaction");
